364892e35021602f0c263ae6bfc23f12e8d5d4bc
docteurZed <amoszotepe11@gmail.com>
2025-05-28 00:38:13 +0000

    Ajout des fils d'attente aux notifications

--- app/Notifications/SystemNotification.php
+++ app/Notifications/SystemNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\HtmlString;
 
-class SystemNotification extends Notification
+class SystemNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
--- app/Notifications/UserActivityNotification.php
+++ app/Notifications/UserActivityNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class UserActivityNotification extends Notification
+class UserActivityNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
