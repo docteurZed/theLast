2cd291aed37523308db9c106bbc76c98a1369577
docteurZed <amoszotepe11@gmail.com>
2025-06-04 15:37:12 +0000

    ....

--- resources/views/participant/notification/index.blade.php
+++ resources/views/participant/notification/index.blade.php
@@ -5,7 +5,7 @@
 <div class="max-w-4xl mx-auto text-gray-400 mb-20">
     @forelse ($discussions as $disc)
         <a href="{{ route('participant.notification.show', ['threadKey' => $disc->thread_key]) }}">
-            <div class="flex items-center {{ !$disc->all_read ? 'bg-gray-700' : 'bg-gray-800' }} shadow-md p-5 cursor-pointer {{ !$loop->last ?? 'border border-gray-700' }}">
+            <div class="flex items-center {{ !$disc->all_read ? 'bg-gray-700' : 'bg-gray-800' }} shadow-md p-5 cursor-pointer {{ !$loop->last ? 'border-b border-gray-700' : '' }}">
                 <div class="relative inline-block shrink-0">
                     @if ($disc->is_interlocutor_anonymous)
                         <img class="w-12 h-12 rounded-full" src="{{ asset('images/user.png') }}" alt="user"/>
@@ -38,7 +38,7 @@
         <p class="text-gray-400 text-sm text-center py-16">Aucune discussion en cours.</p>
     @endforelse
 
-    <div class="fixed end-10 bottom-20 group">
+    <div class="fixed end-6 sm:end-28 bottom-28 sm:bottom-56 group">
         <button type="button" data-tooltip-target="tooltip-message" data-tooltip-placement="left" data-drawer-target="drawer-example" data-drawer-show="drawer-example" aria-controls="drawer-example" class="flex justify-center items-center w-[52px] h-[52px] rounded-full shadow-xs hover:text-white text-gray-300 bg-yellow-700 hover:bg-yellow-600">
             <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
                 <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H4.414a1 1 0 0 0-.707.293L.854 15.146A.5.5 0 0 1 0 14.793zm3.5 1a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1zm0 2.5a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1zm0 2.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1z"/>
@@ -53,17 +53,17 @@
 
     <!-- drawer component -->
     <div id="drawer-example" class="fixed top-0 left-0 z-60 h-screen p-4 overflow-y-auto transition-transform -translate-x-full w-full bg-gray-900" tabindex="-1" aria-labelledby="drawer-label">
-        <h5 id="drawer-label" class="inline-flex items-center mb-4 text-base font-semibold text-gray-500 dark:text-gray-400"><svg class="w-4 h-4 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
-          </svg>Message</h5>
-        <button type="button" data-drawer-hide="drawer-example" aria-controls="drawer-example" class="text-gray-400 bg-transparent rounded-lg text-sm w-8 h-8 absolute top-2.5 end-2.5 flex items-center justify-center hover:bg-yellow-700 hover:text-white" >
+        <button type="button" data-drawer-hide="drawer-example" aria-controls="drawer-example" class="text-gray-400 bg-transparent rounded-lg text-sm w-8 h-8 absolute top-2.5 left-2.5 flex items-center justify-center hover:bg-yellow-700 hover:text-white" >
             <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
             </svg>
             <span class="sr-only">Close menu</span>
         </button>
+        <span id="drawer-label" class="inline-flex items-center mb-4 text-base text-sm font-semibold text-gray-400">
+            Nouveau message
+        </span>
 
-        <div class="mt-5">
+        <div class="mt-5 grid grid-col-1 sm:grid-cols-2">
             <div class="relative mb-5"
                     x-data="{
                         open: false,
