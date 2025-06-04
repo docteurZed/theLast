905fe2252d80d7846899ead601d254b4b4726721
docteurZed <amoszotepe11@gmail.com>
2025-06-04 14:55:56 +0000

    ......

--- resources/views/participant/notification/index.blade.php
+++ resources/views/participant/notification/index.blade.php
@@ -121,6 +121,7 @@
     </div>
 </div>
 
+<script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
 <script>
     const input = document.getElementById('image');
     const previewImage = document.getElementById('image-preview');
