694405455750792bdc3efe9da333a0407b5e1db6
docteurZed <amoszotepe11@gmail.com>
2025-06-21 01:03:05 +0000

    ....

--- resources/views/participant/gold_book/index.blade.php
+++ resources/views/participant/gold_book/index.blade.php
@@ -6,21 +6,21 @@
 @section('content')
 
 <div class="max-w-4xl mx-auto p-4 sm:p-6 space-y-6 text-gray-400 my-8">
-    @forelse ($messages as $message)
     <div class="border border-gray-700 bg-gray-800 p-6 rounded-xl shadow">
-        <p class="text-gray-400 text-center mb-4 italic font-semibold">
-            "
-            {{ $message->message }}
-            "
-        </p>
-        <div class="flex items-center justify-center space-x-4">
-        <img src="{{ $message->user->profile_photo ?? asset('images/user.png') }}" class="w-7 h-7 md:w-12 md:h-12 rounded-full" alt="Image">
-        <span class="font-bold text-white text-sm">{{ $message->user->first_name }} {{ $message->user->name }}</span>
-        </div>
+        @forelse ($messages as $message)
+            <p class="text-gray-400 text-center mb-4 italic font-semibold">
+                "
+                {{ $message->message }}
+                "
+            </p>
+            <div class="flex items-center justify-center space-x-4">
+            <img src="{{ $message->user->profile_photo ?? asset('images/user.png') }}" class="w-7 h-7 md:w-12 md:h-12 rounded-full" alt="Image">
+            <span class="font-bold text-white text-sm">{{ $message->user->first_name }} {{ $message->user->name }}</span>
+            </div>
+        @empty
+            <p class="text-gray-400 italic">Aucun message enrégistré</p>
+        @endforelse
     </div>
-    @empty
-
-    @endforelse
 </div>
 
 @endsection
